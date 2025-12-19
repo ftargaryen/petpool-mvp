ce6518a06dc12e6e3f6eadcd6a79f0a9e0bac66e
santino <franklinifeanyi16@gmail.com>
2025-12-19 22:42:06 +0100

    Fix database KeyError for deployment

--- petpool_project/settings.py
+++ petpool_project/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'petpool_project.wsgi.application'
 # Database configuration for Render (PostgreSQL) and Local (SQLite)
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',
         conn_max_age=600
     )
 }
